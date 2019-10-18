a1f61ef50a0297b59fc147ed33ab986240d7c1a5
Yisel FY <brancyxy@gmail.com>
2019-10-18 05:14:27 +0200

    Input done

---
+++ elsofodulo/Assets/Scripts/InputHandler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class InputHandler : MonoBehaviour
+{
+    [SerializeField]
+    private InputField InSzelepSzam;
+    [SerializeField]
+    private InputField InSzelepSebesseg;
+
+    public static Szelep[] szelepek;
+
+    public void Kezdes()
+    {
+        try
+        {
+            int szelepSzam = int.Parse(InSzelepSzam.text);
+            double szelepSebesseg = double.Parse(InSzelepSebesseg.text);
+
+            if (szelepSzam == 0 || (szelepSebesseg == 0 || szelepSebesseg > 20)) throw new Exception();
+            szelepek = new Szelep[szelepSzam];
+
+            for (int i = 0; i < szelepek.Length; i++) 
+                szelepek[i] = new Szelep(szelepSebesseg);
+        }
+        catch (Exception e) { Debug.Log(e); }
+    }
+}
---
+++ elsofodulo/Assets/Scripts/Szelep.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Szelep
+{
+    public bool bekapcsolva;
+
+    public double folyamSebesseg;
+
+    public Szelep(double meret)
+    {
+        folyamSebesseg = meret;
+    }
+}
